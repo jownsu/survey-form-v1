74af3d41d6273fb9fc780c2d4ea3c9772bf47042
Jownsu <jhonesdigno666@gmail.com>
2022-09-16 00:17:43 +0800

    fixed survery model

--- models/survey.model.js
+++ models/survey.model.js
@@ -5,7 +5,7 @@ const Constants 			= require('../config/constants');
 class SurveyModel extends Model {
 	constructor() {
 		super();
-		this.captcha = null;
+		this.captcha = this.generateCaptcha();        
 	}
 
 	async getSurvey() {
@@ -46,11 +46,15 @@ class SurveyModel extends Model {
 
 	// supply the logic for each function:
 	generateCaptcha(){
-		return ""; 
+		return Math.random() * 999999; 
 	}
 
 	verifyCaptchaInput(input){
-		return ""; 
+        if(input == this.captcha){
+            return "Success! Captcha input matched."; 
+        }else{
+            return "Error! Captcha input doesn't matched.";
+        }
 	}
 }
 
