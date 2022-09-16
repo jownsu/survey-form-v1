0315c9c964fd92269c47a80a76bdabb636d3b625
Jownsu <jhonesdigno666@gmail.com>
2022-09-17 00:51:27 +0800

    fix

--- test/survey.test.js
+++ test/survey.test.js
@@ -4,7 +4,7 @@ const chai                  = require('chai');
 const expect                = chai.expect;
 const SurveyModel           = require('../models/survey.model');
 
-// test comment
+// test commentss
 describe("Survey Model", function(){
 
     it('Should return success message when input captcha matches.', function(){
